ba3e10c8e8b6ebbe168cbf5e5214c90a57123c69
Ivan Egorov <ivan.egorov@cdek.ru>
2020-09-17 12:58:43 +0700

    1-components: 3-category-product-cart passed tests

--- 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
+++ 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
@@ -1,7 +1,8 @@
 import {
   Component,
-  ChangeDetectionStrategy,
+  ChangeDetectionStrategy, Input, Output, EventEmitter,
 } from '@angular/core';
+import {IProduct} from '../../../../../shared/mocks/1-components/product';
 
 @Component({
   selector: 'ngx-shop-content-product',
@@ -9,6 +10,20 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryProductComponent {
+  @Input()
+  public product: IProduct = {} as IProduct;
 
+  @Output()
+  public addToCart: EventEmitter<string> = new EventEmitter<string>();
+
+  @Output()
+  public goToProduct: EventEmitter<string> = new EventEmitter<string>();
+
+  public addToBasket(): void {
+    this.addToCart.emit('Вы добавили товар в корзину');
+  }
+  public redirectTo(): void {
+    this.goToProduct.emit('Переход на компонент товара');
+  }
 
 }
